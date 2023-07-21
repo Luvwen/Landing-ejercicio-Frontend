344aeb718e484a2a1754919afc126e8ced8a6d78
Santiago <santiagobalino@hotmail.com>
2023-07-21 16:31:37 -0300

    End desktop styles and add functionality

---
+++ index.js
@@ -0,0 +1,61 @@
+const titleContainer = document.getElementById('title-container');
+const titleOne = document.getElementById('tablet-title-one');
+const titleTwo = document.getElementById('tablet-title-two');
+const backgroundImage = document.getElementById('background-image');
+
+const boxIcons = document.getElementById('box-icons');
+const checkOne = document.getElementById('check-one');
+const checkTwo = document.getElementById('check-two');
+const checkThree = document.getElementById('check-three');
+const checkFour = document.getElementById('check-four');
+const checkFive = document.getElementById('check-five');
+
+titleContainer.addEventListener('click', (evt) => {
+    if (
+        evt.target.classList[0] === 'tablet-title-two' &&
+        evt.target.classList.contains('gray')
+    ) {
+        titleTwo.classList.remove('gray');
+        titleTwo.classList.add('black');
+        titleOne.classList.remove('black');
+        titleOne.classList.add('gray');
+
+        backgroundImage.classList.toggle('tablet-image');
+    }
+    if (
+        evt.target.classList[0] === 'tablet-title-one' &&
+        evt.target.classList.contains('gray')
+    ) {
+        titleTwo.classList.remove('black');
+        titleTwo.classList.add('gray');
+        titleOne.classList.remove('gray');
+        titleOne.classList.add('black');
+        backgroundImage.classList.toggle('tablet-image');
+    }
+});
+
+let arrOfChecks = [
+    { check: checkOne, pressed: false, element: 'one' },
+    { check: checkTwo, pressed: false, element: 'two' },
+    { check: checkThree, pressed: false, element: 'three' },
+    { check: checkFour, pressed: false, element: 'four' },
+    { check: checkFive, pressed: false, element: 'five' },
+];
+boxIcons.addEventListener('click', (evt) => {
+    arrOfChecks.map((el) =>
+        el.check.id === evt.target.id && el.pressed === false
+            ? (el.pressed = true)
+            : (el.pressed = false)
+    );
+    arrOfChecks.map((el) => {
+        if (el.pressed === true) {
+            el.check.classList.add('highlight');
+            const element = document.getElementById(el.element);
+            element.classList.add(el.element);
+        } else {
+            el.check.classList.remove('highlight');
+            const element = document.getElementById(el.element);
+            element.classList.remove(el.element);
+        }
+    });
+});
